e04d322222d3c07e6821dc2e7b3ad1680f3603e2
Anna Kozar <91608016+annakozar@users.noreply.github.com>
2022-03-27 09:24:14 +0200

    Update exercise_2.py
    Added an exercise description.

--- 03_conditionals/exercise_2.py
+++ 03_conditionals/exercise_2.py
@@ -1,3 +1,6 @@
+# Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. 
+# The following shows two executions of the program:
+
 raw_hours = input('Enter Hours\n')
 try:
     hours = int(raw_hours)
