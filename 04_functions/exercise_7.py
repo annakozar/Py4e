9b2eff0eb1c8b72c90878c45555b3a195d2bb888
Ann <ann@example.com>
2022-03-28 11:38:50 +0000

    rewrite first line into comment

--- 04_functions/exercise_7.py
+++ 04_functions/exercise_7.py
@@ -1,4 +1,4 @@
-Rewrite the grade program from the previous chapter using a function called computegrade that takes a score as its parameter and returns a grade as a string
+#Rewrite the grade program from the previous chapter using a function called computegrade that takes a score as its parameter and returns a grade as a string
 
 def computegrade(grade):
     if grade > 1.0 :
